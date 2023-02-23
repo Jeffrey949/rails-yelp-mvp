ebee5553dada2f0eb5a097f9e550e5641214c09e
Jeffrey Boateng <jeff05@gmx.net>
2023-02-23 08:44:17 +0100

    show & Index done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'french' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'french' }
+burger_king = { name: 'Burger_king', address: 'Hohenzollernring 18', category: 'french' }
+pasta_bella = { name: 'pasta_bella', address: 'rome', category: 'italian' }
+lakis = { name: 'lakis', address: 'düsseldorf', category: 'italian' }
+
+[dishoom, pizza_east, burger_king, pasta_bella, lakis].each do |rest|
+  restaurant = Restaurant.create!(rest)
+  puts "Created #{restaurant.name}"
+end
